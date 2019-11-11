8d54b8f86591e326a9ff26d3dc483672d062a9e1
Николай Донцов <nikolay.dontsov@yandex.ru>
2019-11-11 16:01:01 +0300

    first creation WebStore

---
+++ WebStoreASP/Startup.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace WebStoreASP
+{
+    public class Startup
+    {
+        
+        IConfiguration Configuration { get; set; }
+        public Startup(IConfiguration Config) => Configuration = Config;
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddMvc();
+        }
+
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseMvc(routes =>
+            {
+                routes.MapRoute(
+                    "default",
+                    "{controller=Home}/{action=Index}/{id?}");
+            });
+        }
+    }
+}
---
+++ WebStoreASP/ViewModels/EmployeeView.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebStoreASP.ViewModels
+{
+    public class EmployeeView
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public DateTime BirthDay { get; set; }
+        public DateTime HiringDate { get; set; }
+        public int Age => (DateTime.Now - BirthDay).Days/365;
+    }
+}
