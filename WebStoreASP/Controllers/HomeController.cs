facdd7a910bf3a9637b88732f2bd9f826003e8aa
Николай Донцов <nikolay.dontsov@yandex.ru>
2019-11-14 09:42:15 +0300

    adding page Infrastructure

--- WebStoreASP/Controllers/HomeController.cs
+++ WebStoreASP/Controllers/HomeController.cs
@@ -15,7 +15,50 @@ namespace WebStoreASP.Controllers
             return View();
         }
 
-        
+        public IActionResult Page404()
+        {
+            return View();
+        }
+
+        public IActionResult Blog()
+        {
+            return View();
+        }
+
+        public IActionResult BlogSingle()
+        {
+            return View();
+        }
+
+        public IActionResult Cart()
+        {
+            return View();
+        }
+
+        public IActionResult Checkout()
+        {
+            return View();
+        }
+
+        public IActionResult ContactUs()
+        {
+            return View();
+        }
+
+        public IActionResult Login()
+        {
+            return View();
+        }
+
+        public IActionResult ProductDetails()
+        {
+            return View();
+        }
+
+        public IActionResult Shop()
+        {
+            return View();
+        }
 
     }
 }
