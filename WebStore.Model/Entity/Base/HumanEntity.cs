7f39e5bfad014790b0c55ccf8c1ec4b3cdd8951f
Николай Донцов <nikolay.dontsov@yandex.ru>
2019-11-25 16:15:56 +0300

    Change Migration

--- WebStore.Model/Entity/Base/HumanEntity.cs
+++ WebStore.Model/Entity/Base/HumanEntity.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Text;
 
 namespace WebStore.Model.Entity.Base
@@ -7,12 +9,16 @@ namespace WebStore.Model.Entity.Base
     public abstract class HumanEntity : BaseEntity
     {
         private string _firstName;
+        [Column(TypeName = "nvarchar(200)")]
+        [Required]
         public string FirstName
         {
             get => _firstName;
             set => _firstName = value;
         }
         private string _lastName;
+        [Column(TypeName = "nvarchar(200)")]
+        [Required]
         public string LastName
         {
             get => _lastName;
--- WebStore.Model/Entity/Base/NamedEntity.cs
+++ WebStore.Model/Entity/Base/NamedEntity.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Text;
 
 namespace WebStore.Model.Entity.Base
@@ -7,6 +8,7 @@ namespace WebStore.Model.Entity.Base
     public abstract class NamedEntity : BaseEntity
     {
         private string _name;
+        [Required]
         public string Name
         {
             get => _name;
