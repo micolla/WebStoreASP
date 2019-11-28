d18b81b4bcbbfc7c62b132a277fd71de99662d5a
Николай Донцов <nikolay.dontsov@yandex.ru>
2019-11-28 12:45:15 +0300

    Add Identity middleware

--- WebStore.DAL/WebStoreDBContext.cs
+++ WebStore.DAL/WebStoreDBContext.cs
@@ -1,12 +1,11 @@
-﻿using Microsoft.EntityFrameworkCore;
-using System;
-using System.Collections.Generic;
-using System.Text;
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
 using WebStore.Model.Entity;
+using WebStore.Model.Entity.Identity;
 
 namespace WebStore.DAL.SQLDBData
 {
-    public class WebStoreDBContext : DbContext
+    public class WebStoreDBContext : IdentityDbContext<User, Role, string>
     {
         public DbSet<Employee> Employees { get; set; }
         public DbSet<Brand> Brands { get; set; }
---
+++ WebStore.Model/Entity/Identity/User.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace WebStore.Model.Entity.Identity
+{
+    public class User : IdentityUser { }
+}
