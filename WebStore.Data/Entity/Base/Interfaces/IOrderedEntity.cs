1f3d33613e4b1e325f922ad8402fd9dc622987cf
Николай Донцов <nikolay.dontsov@yandex.ru>
2019-12-02 21:02:09 +0300

    drop InmemoryData project

--- WebStore.Data/Entity/Base/Interfaces/IOrderedEntity.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace WebStore.Data.Entity.Base.Interfaces
-{
-    public interface IOrderedEntity : IBaseEntity
-    {
-        /// <summary>Порядковый номер</summary>
-        int Order { get; set; }
-    }
-}
--- WebStore.Data/Interfaces/IEmployeeDataProvider.cs
+++
@@ -1,9 +0,0 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-using WebStore.Data.Entity;
-
-namespace WebStore.Data.Interfaces
-{
-    public interface IEmployeeDataProvider : IDataProvider<Employee> { }
-}
